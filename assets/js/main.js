e5fda67c5124edfdb304d9dea75af65b8c42c9e6
tiago-sb <belatiago72@gmail.com>
2024-02-05 18:19:02 -0300

    adicona o arquivo main, final

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li onclick="selected(${pokemon.number})" class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
